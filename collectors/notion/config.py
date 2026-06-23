00c05eac249ea986a733b57f49e97692baa93cd0
Shivam <83861668+5h1Vm@users.noreply.github.com>
2026-06-23 19:43:49 +0530

    Expand Notion runtime configuration

--- collectors/notion/config.py
+++ collectors/notion/config.py
@@ -5,9 +5,26 @@ PROFILE_DIR = CFG.get(
     "state/notion/profile"
 )
 
+PROFILE_NAME = CFG.get(
+    "NOTION_PROFILE_NAME",
+    "Default"
+)
+
 DOWNLOAD_DIR = CFG.get(
     "NOTION_DOWNLOAD_DIR",
     "/home/hc/Downloads"
+)
+
+WORKSPACE_URL = CFG.get(
+    "NOTION_WORKSPACE_URL",
+    ""
+)
+
+POLL_INTERVAL_SECONDS = int(
+    CFG.get(
+        "NOTION_POLL_INTERVAL_SECONDS",
+        "60"
+    )
 )
 
 EXPORT_TIMEOUT_MINUTES = int(
