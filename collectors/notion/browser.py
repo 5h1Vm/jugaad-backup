94fcc93ccd1b89fe05d9f54e8666425b2df0563d
Shivam <83861668+5h1Vm@users.noreply.github.com>
2026-06-23 19:59:34 +0530

    Add browser workflow stages for Notion export

--- collectors/notion/browser.py
+++ collectors/notion/browser.py
@@ -19,19 +19,31 @@ class NotionBrowser:
     def __init__(self, download_dir: Path):
         self.download_dir = Path(download_dir)
 
+    def open_workspace(self):
+        print(f'[+] Opening workspace: {WORKSPACE_URL}')
+
+    def open_export_dialog(self):
+        print('[+] Opening Actions menu')
+        print('[+] Opening Export dialog')
+
+    def configure_export(self, export_format: str):
+        print(f'[+] Export format: {export_format}')
+        print('[+] Database views: Default view')
+        print('[+] Page content: Everything')
+        print('[+] Include subpages: ON')
+
     def _run_export(self, export_format: str):
 
         existing = export_files(self.download_dir)
         known_count = len(existing)
 
-        print(f'[+] Starting Notion export: {export_format}')
         print(f'[+] Workspace URL: {WORKSPACE_URL}')
         print(f'[+] Profile dir: {PROFILE_DIR}')
         print(f'[+] Profile name: {PROFILE_NAME}')
-        print('[+] Include subpages: ON')
-        print('[+] Database views: Default view')
-        print('[+] Page content: Everything')
-        print('[+] Waiting for new export ZIP')
+
+        self.open_workspace()
+        self.open_export_dialog()
+        self.configure_export(export_format)
 
         archive = wait_for_new_export(
             self.download_dir,
