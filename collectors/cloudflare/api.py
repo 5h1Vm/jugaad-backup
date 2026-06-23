ce34639fca016005eb53efea8172979c14ceabc0
Shivam <83861668+5h1Vm@users.noreply.github.com>
2026-06-23 14:16:43 +0530

    Use package imports in Cloudflare API

--- collectors/cloudflare/api.py
+++ collectors/cloudflare/api.py
@@ -1,37 +1,18 @@
 import requests
-import importlib.util
 
-spec = importlib.util.spec_from_file_location(
-    "cloudflare_config",
-    "/opt/backup/collectors/cloudflare/config.py"
-)
-
-cf_config = importlib.util.module_from_spec(spec)
-
-spec.loader.exec_module(
-    cf_config
-)
+from . import config as cf_config
 
 ENV = cf_config.ENV
 
 
 def headers():
-
     return {
-        "Authorization":
-        f"Bearer {ENV['CLOUDFLARE_API_TOKEN']}",
-        "Content-Type":
-        "application/json"
+        "Authorization": f"Bearer {ENV['CLOUDFLARE_API_TOKEN']}",
+        "Content-Type": "application/json"
     }
 
 
 def get(url):
-
-    r = requests.get(
-        url,
-        headers=headers()
-    )
-
+    r = requests.get(url, headers=headers())
     r.raise_for_status()
-
     return r.json()
